45ff2d0e91a08c16ed194db22c146b1796040606
christopher cahill and maly phongsavanh <christopher@>
2018-12-05 11:13:50 -0800

    write save method and add ==()

--- lib/books.rb
+++ lib/books.rb
@@ -11,7 +11,7 @@ attr_accessor :title, :book_id
     books = []
     returned_books.each() do |book|
       title = book.fetch("title")
-      id = book.fetch("book_id")
+      book_id = book.fetch("book_id").to_i()
       books.push(Book.new({:title => title, :book_id => book_id}))
   end
     books
@@ -20,5 +20,14 @@ attr_accessor :title, :book_id
   # def self.test_method_books
   #   books = DB.exec("SELECT * FROM books;")
   # end
+
+  def save
+    result = DB.exec("INSERT INTO books (title, book_id) VALUES ('#{@title}',#{@book_id}) RETURNING book_id;")
+    @book_id = result.first().fetch("book_id").to_i()
+  end
+
+  def ==(another_book)
+    self.title().==(another_book.title()).&(self.book_id().==(another_book.book_id()))
+  end
 
 end
--- spec/books_spec.rb
+++ spec/books_spec.rb
@@ -10,4 +10,27 @@ require("spec_helper")
     end
   end
 
+  describe("#title") do
+  it("tells you its title") do
+    book = Book.new({:title => "Treasure Island", :book_id => 3})
+    expect(book.title()).to(eq("Treasure Island"))
+  end
+end
+
+describe("#id") do
+  it("sets its book ID when you save it") do
+    book = Book.new({:title => "A Christmas Carol", :book_id => 3})
+    book.save()
+    expect(book.book_id()).to(be_an_instance_of(Integer))
+  end
+end
+
+describe("#save") do
+  it("lets you save books to the database") do
+    book = Book.new({:title => "Treasure Island", :book_id =>3})
+    book.save()
+    expect(Book.all()).to(eq([book]))
+  end
+end
+
 end
